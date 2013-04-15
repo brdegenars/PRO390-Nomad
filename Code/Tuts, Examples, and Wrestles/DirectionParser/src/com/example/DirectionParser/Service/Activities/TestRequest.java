4408cc62ac711a69b5fa800ae50cd01cffdb1607
brdegenars <bradleydegenaars@gmail.com>
2013-04-15 16:32:09 -0600

    Got the app to launch in my emulator. It currently crashes due to the URL being constructed incorrectly. Need to fix the way the parameters are extracted and then compiled into a single url.

---
+++ Code/Tuts, Examples, and Wrestles/DirectionParser/src/com/example/DirectionParser/Service/Activities/TestRequest.java
@@ -0,0 +1,33 @@
+package com.example.DirectionParser.Service.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.example.DirectionParser.R;
+import com.example.DirectionParser.Service.Service.ServiceRequest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: brdegenaars
+ * Date: 4/15/13
+ * Time: 3:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestRequest extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        final ServiceRequest serviceRequest = new ServiceRequest();
+
+        Button testButton = (Button)this.findViewById(R.id.testButton);
+
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                serviceRequest.request("Toronto","New York",false, null, null, "metric");
+            }
+        });
+    }
+}
