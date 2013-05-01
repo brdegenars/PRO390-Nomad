a438c4b6c38f4c4aac85e2783711fac4ac1538c0
brdegenars <bradleydegenaars@gmail.com>
2013-05-01 12:34:16 -0600

    Working on getting the Hot pad's long press to bring up application list. Bound  all the hot pads to the same listener so that changes are made to all hot pads, in one place.

--- Code/Nomad/Nomad-1.0/src/com/example/Nomad/MyActivity.java
+++ Code/Nomad/Nomad-1.0/src/com/example/Nomad/MyActivity.java
@@ -6,22 +6,51 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import java.util.List;
+
 public class MyActivity extends Activity
 {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+        // Creation and instantiation of ImageButtons (hot pads)
+        ImageButton topLeft, topCenter, topRight, bottomLeft, bottomCenter, bottomRight;
+
+        topLeft = (ImageButton)this.findViewById(R.id.imageButton_home_topLeft);
+        topCenter = (ImageButton)this.findViewById(R.id.imageButton_home_topCenter);
+        topRight = (ImageButton)this.findViewById(R.id.imageButton_home_topRight);
+
+        bottomLeft = (ImageButton)this.findViewById(R.id.imageButton_home_bottomLeft);
+        bottomCenter = (ImageButton)this.findViewById(R.id.imageButton_home_bottomCenter);
+        bottomRight = (ImageButton)this.findViewById(R.id.imageButton_home_bottomRight);
+
+        topLeft.setOnLongClickListener(onLongClickListener);
+        topCenter.setOnLongClickListener(onLongClickListener);
+        topRight.setOnLongClickListener(onLongClickListener);
+
+        bottomLeft.setOnLongClickListener(onLongClickListener);
+        bottomCenter.setOnLongClickListener(onLongClickListener);
+        bottomRight.setOnLongClickListener(onLongClickListener);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-        ImageButton topLeft = (ImageButton)this.findViewById(R.id.imageButton_home_topLeft);
-        topLeft.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                Intent.createChooser(new Intent(Intent.ACTION_CHOOSER), "Select Application");
-                return true;
-            }
-        });
     }
+
+    private View.OnLongClickListener onLongClickListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+
+            /* TODO: This is where the logic for displaying the list of available apps for hot pads is
+               TODO: work on this...
+             */
+
+            Intent intent = new Intent(Intent.ACTION_MAIN, null);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+            List appList = getPackageManager().queryIntentActivities(intent, 0);
+
+            //Intent.createChooser(new Intent(Intent.ACTION_CHOOSER), "Select Application");
+            return true;
+        }
+    };
 }
