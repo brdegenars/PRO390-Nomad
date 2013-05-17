f681c740ab9b7086e2cfe0edfe9ebd4737e8c3cb
brdegenars <bradleydegenaars@gmail.com>
2013-05-17 15:01:22 -0600

    Edited main layout to reflect the proper appearance of the navigation utility's single destination input. Working on getting the origin and destination from their respective input fields so I can then send them to the google service and receive back the map data. After that will be the task of building the map and displaying it.

--- Code/Nomad/NomadNavigation/src/com/activites/NavigationPrompt.java
+++ Code/Nomad/NomadNavigation/src/com/activites/NavigationPrompt.java
@@ -1,9 +1,16 @@
 package com.activites;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NavigationPrompt extends Activity {
+
+    private EditText originInput, destinationInput;
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +18,20 @@ public class NavigationPrompt extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        originInput = (EditText)this.findViewById(R.id.originInput);
+        destinationInput = (EditText)this.findViewById(R.id.destinationInput);
+
+        Button navigateMe = (Button)this.findViewById(R.id.navigateMe);
+        navigateMe.setOnClickListener(navigateMeOnClickListener);
+        // navigateMe.setEnabled(false);
     }
+
+    private View.OnClickListener navigateMeOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // TODO: Take origin and destination, send to Google direction service, display map.
+            originInput.getText();
+        }
+    };
 }
