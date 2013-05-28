6393e62df46d6f27a3f814a42f223c08218652d7
brdegenars <bradleydegenaars@gmail.com>
2013-05-28 17:59:52 -0600

    Successfully requests JSON object and receives  expected results from service.

--- Code/Nomad/NomadNavigation/src/com/activities/MapHandler.java
+++ Code/Nomad/NomadNavigation/src/com/activities/MapHandler.java
@@ -11,6 +11,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import service.DirectionRequest;
+import service.DirectionURLGenerator;
 
 
 /**
--- Code/Nomad/NomadNavigation/src/com/activities/NavigationPrompt.java
+++ Code/Nomad/NomadNavigation/src/com/activities/NavigationPrompt.java
@@ -8,7 +8,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import observer.LocationTextObserver;
+import org.json.JSONObject;
+import service.DirectionRequest;
+import service.DirectionURLGenerator;
 import subject.LocationTextSubject;
+
+import java.util.concurrent.ExecutionException;
 
 public class NavigationPrompt extends Activity {
 
@@ -40,20 +45,45 @@ public class NavigationPrompt extends Activity {
         destinationSubject.registerObserver(locationTextObserver);
     }
 
+    @Override
+    public void onResume(){
+
+    }
+
+    @Override
+    public void onPause(){
+
+    }
+
     private View.OnClickListener navigateMeOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            // TODO: Take origin and destination, send to Google direction service, display map.
+
+            String originValue = originInput.getText().toString();
+            String destiniationValue = destinationInput.getText().toString();
+
             System.out.println("INPUTS: ");
-            System.out.println("Origin : " + originInput.getText());
-            System.out.println("Destination : " + destinationInput.getText());
+            System.out.println("Origin : " + originValue);
+            System.out.println("Destination : " + destiniationValue);
 
             SharedPreferences localData = getPreferences(Activity.MODE_PRIVATE);
             SharedPreferences.Editor localDataEditor = localData.edit();
 
-            localDataEditor.putString("origin", originInput.getText().toString());
-            localDataEditor.putString("destination", destinationInput.getText().toString());
+            localDataEditor.putString("origin", originValue);
+            localDataEditor.putString("destination", destiniationValue);
 
+            DirectionRequest sendRequest = new DirectionRequest();
+            JSONObject directionJSONObject;
+
+            try {
+                directionJSONObject = sendRequest.execute(DirectionURLGenerator.generateURL(originValue, destiniationValue)).get();
+            } catch (InterruptedException e) {
+                System.out.println("ERROR : Current thread was interrupted");
+            } catch (ExecutionException e) {
+                System.out.println("ERROR: AsyncTask was unable to execute successfully.");
+            }
+
+            // TODO: Take directionJSONObject and send it off to be constructed into a map with directions from the JSONObject
             Intent launchMapIntent = new Intent(v.getContext(), MapHandler.class);
             startActivity(launchMapIntent);
         }
